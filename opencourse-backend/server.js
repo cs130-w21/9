d42d3cb5db9d4b02780576ee8e18eb23578c3193
Dylan <dbreon@ucla.edu>
2021-01-26 20:30:36 -0600

    set up express with placeholder api calls to later fill in with database requests

--- opencourse-backend/server.js
+++ opencourse-backend/server.js
@@ -2,11 +2,19 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { json } = require("body-parser");
 const PORT = 4000;
+const courseRoutes = express.Router();
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.listen(PORT, function () {
   console.log("Server is running on Port: " + PORT);
+});
+
+app.use("/courses", courseRoutes);
+
+courseRoutes.route("/").get((req, res) => {
+  console.log("Displaying all courses!");
 });
