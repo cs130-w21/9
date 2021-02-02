c673d638c126df0be6bea2c07bb4fd054816d6ce
jasonhuan <sino@g.ucla.edu>
2021-02-02 09:17:51 -0800

    setup express

--- opencourse-backend/server.js
+++ opencourse-backend/server.js
@@ -7,6 +7,18 @@ const PORT = 4000;
 app.use(cors());
 app.use(bodyParser.json());
 
+const courseRoutes = express.Router();
+
+app.use("/courses", courseRoutes);
+
 app.listen(PORT, function () {
   console.log("Server is running on Port: " + PORT);
+});
+
+courseRoutes.route("/").get(function (req, res) {
+  console.log("testing");
+});
+
+courseRoutes.route("/:id").get(function (req, res) {
+  console.log("testing");
 });
