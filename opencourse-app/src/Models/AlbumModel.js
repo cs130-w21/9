bc6fb0c92d4ecdd9f5fbc803aae94a0e13a9e37c
Grant Roberts <groberts96@gmail.com>
2021-02-10 11:44:20 -0800

    Added in model to work with in the future.

--- opencourse-app/src/Models/AlbumModel.js
+++ opencourse-app/src/Models/AlbumModel.js
@@ -1,4 +1,4 @@
-export class AlbumModel {
+class AlbumModel {
     constructor() {
       this.cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     }
@@ -7,4 +7,6 @@ export class AlbumModel {
         return this.cards;
     }
   }
+
+  export default AlbumModel;
  
