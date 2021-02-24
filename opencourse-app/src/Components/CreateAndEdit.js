eff6cce986411074ebfca4316d53e69054e2b268
Dylan <dbreon@ucla.edu>
2021-02-24 13:11:41 -0800

    fixed merge conflicts with master

--- opencourse-app/src/Components/CreateAndEdit.js
+++ opencourse-app/src/Components/CreateAndEdit.js
@@ -1,32 +1,14 @@
 import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
-<<<<<<< HEAD
-import Container from "@material-ui/core/Container";
-=======
->>>>>>> cbe266e35b69bc8a3b3446def23c8afdeb6b150c
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
-<<<<<<< HEAD
-import CardMedia from "@material-ui/core/CardMedia";
-=======
->>>>>>> cbe266e35b69bc8a3b3446def23c8afdeb6b150c
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-<<<<<<< HEAD
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import ShareIcon from "@material-ui/icons/Share";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
-import Button from "@material-ui/core/Button";
-
-export default function CreateAndEdit() {
-  const classes = useStyles();
-=======
 import ShareIcon from "@material-ui/icons/Share";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import Button from "@material-ui/core/Button";
@@ -38,21 +20,11 @@ export default function CreateAndEdit() {
   const classes = useStyles();
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
->>>>>>> cbe266e35b69bc8a3b3446def23c8afdeb6b150c
   const [modules, setModules] = useState([
     {
       id: 1,
       title: "first modulee",
       desc: "this is the first module",
-<<<<<<< HEAD
-      link: "www.google.com",
-    },
-  ]);
-
-  const newModule = () => {
-    const list = modules.concat({
-      id: modules.length + 1,
-=======
       link: "https://pictureofahotdog.com/",
     },
   ]);
@@ -96,7 +68,6 @@ export default function CreateAndEdit() {
   const newModule = () => {
     const list = modules.concat({
       id: modules.length + 1, //need to change this to the id in db.
->>>>>>> cbe266e35b69bc8a3b3446def23c8afdeb6b150c
       title: "",
       desc: "",
       link: "",
@@ -105,38 +76,6 @@ export default function CreateAndEdit() {
     setModules(list);
   };
 
-<<<<<<< HEAD
-  const modulesList = modules.map((module, index) => (
-    <Grid item className={module.moduleCard}>
-      <Card className={module.moduleCard}>
-        <CardHeader
-          avatar={<Avatar aria-label="recipe">{index + 1}</Avatar>}
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title={module.title}
-          // subheader="September 14, 2016"
-        />
-        <CardContent>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {module.desc}
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          {/* <IconButton aria-label="add to favorites">
-            <FavoriteIcon />
-          </IconButton> */}
-          <IconButton aria-label="share">
-            <ShareIcon />
-          </IconButton>
-          {/* <IconButton aria-label="show more">
-            <ExpandMoreIcon />
-          </IconButton> */}
-        </CardActions>
-      </Card>
-=======
   const deleteModule = (id) => {
     console.log("deleting", id);
     const newList = modules.filter((item) => item.id !== id);
@@ -152,7 +91,6 @@ export default function CreateAndEdit() {
         deleteModule={deleteModule}
         openModal={handleOpen}
       />
->>>>>>> cbe266e35b69bc8a3b3446def23c8afdeb6b150c
     </Grid>
   ));
 
@@ -165,16 +103,6 @@ export default function CreateAndEdit() {
         height="100vh"
         className={classes.grid}
       >
-<<<<<<< HEAD
-        <div className={classes.submitButton}>
-          <Grid item>
-            <Button variant="contained" color="primary">
-              Save
-            </Button>
-          </Grid>
-        </div>
-        <Grid item>
-=======
         <Modal
           open={open}
           onClose={handleClose}
@@ -232,17 +160,11 @@ export default function CreateAndEdit() {
           <Typography variant="h4">Course Title:</Typography>
         </Grid>
         <Grid item className={classes.gridItem}>
->>>>>>> cbe266e35b69bc8a3b3446def23c8afdeb6b150c
           <TextField
             className={classes.textInput}
             id="outlined-basic"
             label="Course Title"
             variant="outlined"
-<<<<<<< HEAD
-          />
-        </Grid>
-        <Grid item>
-=======
             defaultValue={title}
             onChange={(e) => setTitle(e.target.value)}
           />
@@ -251,18 +173,11 @@ export default function CreateAndEdit() {
           <Typography variant="h6">Course Description:</Typography>
         </Grid>
         <Grid item className={classes.gridItem} style={{ marginTop: "1%" }}>
->>>>>>> cbe266e35b69bc8a3b3446def23c8afdeb6b150c
           <TextField
             className={classes.textInput}
             id="outlined-basic"
             label="Course Decription"
             variant="outlined"
-<<<<<<< HEAD
-            multiline
-            rows={4}
-          />
-        </Grid>
-=======
             defaultValue={desc}
             multiline
             rows={4}
@@ -272,22 +187,17 @@ export default function CreateAndEdit() {
         <Grid item style={{ width: "90%" }}>
           <Typography variant="h6">Course Modules:</Typography>
         </Grid>
->>>>>>> cbe266e35b69bc8a3b3446def23c8afdeb6b150c
         <Grid container direction="column" alignItems="center">
           {modulesList}
         </Grid>
         <Grid item>
           <Button
-<<<<<<< HEAD
-            style={{ margin: 20 }}
-=======
             style={{
               margin: 20,
               background:
                 "radial-gradient(circle, rgba(0,212,255,1) 30%, rgba(39,74,252,1) 100%)",
               color: "white",
             }}
->>>>>>> cbe266e35b69bc8a3b3446def23c8afdeb6b150c
             onClick={newModule}
             variant="contained"
             color="primary"
@@ -300,11 +210,7 @@ export default function CreateAndEdit() {
   );
 }
 
-<<<<<<< HEAD
-const useStyles = makeStyles({
-=======
 const useStyles = makeStyles((theme) => ({
->>>>>>> cbe266e35b69bc8a3b3446def23c8afdeb6b150c
   root: {
     display: "flex",
     height: "100vh",
@@ -318,26 +224,15 @@ const useStyles = makeStyles((theme) => ({
     marginTop: "5%",
   },
   textInput: {
-<<<<<<< HEAD
-    margin: "3%",
-    width: 600,
-  },
-  submitButton: {
-    width: 600,
-=======
     width: "100%",
   },
   gridItem: { margin: "3%", width: "90%" },
   submitButton: {
     width: "90%",
->>>>>>> cbe266e35b69bc8a3b3446def23c8afdeb6b150c
     display: "flex",
     flexDirection: "row",
     justifyContent: "flex-end",
   },
-<<<<<<< HEAD
-  moduleCard: { margin: "3%", width: 900 },
-=======
   moduleCard: { margin: 10, width: "90%" },
   paper: {
     border: "2px solid #000",
@@ -348,14 +243,9 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
   },
->>>>>>> cbe266e35b69bc8a3b3446def23c8afdeb6b150c
   container: {
     background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
     border: 0,
     borderRadius: 3,
   },
-<<<<<<< HEAD
-});
-=======
 }));
->>>>>>> cbe266e35b69bc8a3b3446def23c8afdeb6b150c
