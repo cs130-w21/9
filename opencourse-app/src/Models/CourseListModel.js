6e9bc48e1c2bc1f5cffa1bc8b340f34357c8011f
Grant Roberts <groberts96@gmail.com>
2021-02-24 08:12:52 -0800

    Added in descriptions for page loading and course titles

--- opencourse-app/src/Models/CourseListModel.js
+++ opencourse-app/src/Models/CourseListModel.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+
+
 class CourseListModel {
     constructor() {
       this.cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -6,6 +9,16 @@ class CourseListModel {
     {
         return this.cards;
     }
+    getData = async () => {
+      try {
+          const data = await axios.get(
+              "http://localhost:4000/courses"
+          );
+            return data.data
+      } catch(e) {
+          console.log(e);
+      }
+  };
   }
 
   export default CourseListModel;
