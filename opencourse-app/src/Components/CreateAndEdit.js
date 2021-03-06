8b269118f58fe2441deaabbc746f81e754583664
dbreon88 <37003326+dbreon88@users.noreply.github.com>
2021-03-05 16:06:16 -0800

    Add model calls to edit create (#82)
    * changed model func so that we feed in all course modules to save. added create course function call to coursepage.js
    
    * modified model and params, added function to route to create course page from course list
    
    * calls to create and edit now work. also changed button to conditional save / edit
    
    * fixed namming error with module desc. leading to it not being displayed
    
    * redirection to home after create or edit

--- opencourse-app/src/Components/CreateAndEdit.js
+++ opencourse-app/src/Components/CreateAndEdit.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import history from "../history.js";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
@@ -15,30 +17,56 @@ import Button from "@material-ui/core/Button";
 import DeleteIcon from "@material-ui/icons/Delete";
 import ModuleCard from "./ModuleCard";
 import Modal from "@material-ui/core/Modal";
+import CoursePageModel from "../Models/CoursePageModel.js";
+import createEditCourseModel from "../Models/createEditCourseModel.js";
+
+const coursePageModel = new CoursePageModel();
+const createEditModel = new createEditCourseModel();
 
 export default function CreateAndEdit() {
   const classes = useStyles();
-  const [title, setTitle] = useState("");
+  const location = useLocation();
+  const courseId = location.state ? location.state.detail : undefined; //undefined if creating course
+  const [editing, setEditing] = useState(!(location.state === undefined));
+  const [title, setTitle] = useState();
   const [desc, setDesc] = useState("");
-  const [modules, setModules] = useState([
-    {
-      id: 1,
-      title: "first modulee",
-      desc: "this is the first module",
-      link: "https://pictureofahotdog.com/",
-    },
-  ]);
+  const [author, setAuthor] = useState();
+  const [modules, setModules] = useState([]);
+
+  const getCourse = async () => {
+    try {
+      const data = await coursePageModel.getData(courseId);
+      console.log(data);
+      if (data.body !== undefined) {
+        setModules(data.body);
+      }
+      if (data.description !== undefined) {
+        setDesc(data.description);
+      }
+      if (data.name !== undefined) {
+        setTitle(data.name);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   const [open, setOpen] = useState(false);
   const [currModule, setCurrModule] = useState({
     id: undefined,
     title: "",
-    desc: "",
+    description: "",
     link: "",
   });
   const [currModuleIndex, setCurrModuleIndex] = useState(0);
 
-/*
+  useEffect(() => {
+    console.log("editing:", editing);
+    if (editing) {
+      getCourse();
+    }
+  }, []);
+  /*
 Function for adding/creating a course. Must fill in all parameters.
 Create the courseEdit model first like this
 const courseListModel = new CourseListModel();
@@ -79,13 +107,46 @@ Example call createCourse("yaboi", "id", "1 minute", "Abstract", [{title: "link2
     }
 */
 
+  const createCourse = () => {
+    if (!title) {
+      alert("You must add a course title!");
+    } else if (modules.length == 0) {
+      alert("You must add course modules");
+    } else {
+      try {
+        createEditModel.createCourse(author, desc, "", title, modules);
+        history.push({
+          pathname: "/",
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    //route back to home
+  };
+
+  const saveEdits = () => {
+    if (modules.length == 0) {
+      alert("You must add course modules");
+    } else {
+      try {
+        createEditModel.editCourse(courseId, modules);
+        history.push({
+          pathname: "/",
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    //route back to home
+  };
 
   const handleModuleTitleChange = (e) => {
     setCurrModule({ ...currModule, title: e.target.value });
   };
 
   const handleModuleDescChange = (e) => {
-    setCurrModule({ ...currModule, desc: e.target.value });
+    setCurrModule({ ...currModule, description: e.target.value });
   };
   const handleModuleLinkChange = (e) => {
     setCurrModule({ ...currModule, link: e.target.value });
@@ -111,7 +172,7 @@ Example call createCourse("yaboi", "id", "1 minute", "Abstract", [{title: "link2
     const list = modules.concat({
       id: modules.length + 1, //need to change this to the id in db.
       title: "",
-      desc: "",
+      description: "",
       link: "",
     });
 
@@ -170,7 +231,7 @@ Example call createCourse("yaboi", "id", "1 minute", "Abstract", [{title: "link2
               id="outlined-basic"
               label="Module Description"
               variant="outlined"
-              defaultValue={currModule.desc}
+              defaultValue={currModule.description}
               style={{ margin: "1%" }}
               onChange={handleModuleDescChange}
               multiline
@@ -187,16 +248,31 @@ Example call createCourse("yaboi", "id", "1 minute", "Abstract", [{title: "link2
           </div>
         </Modal>
         <Grid item className={classes.submitButton}>
-          <Button
-            variant="contained"
-            style={{
-              background: "linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)",
-              color: "white",
-              boxShadow: "0 3px 5px 2px rgba(33, 203, 243, .4)",
-            }}
-          >
-            Save Changes
-          </Button>
+          {editing ? (
+            <Button
+              variant="contained"
+              style={{
+                background: "linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)",
+                color: "white",
+                boxShadow: "0 3px 5px 2px rgba(33, 203, 243, .4)",
+              }}
+              onClick={saveEdits}
+            >
+              Save Changes
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              style={{
+                background: "linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)",
+                color: "white",
+                boxShadow: "0 3px 5px 2px rgba(33, 203, 243, .4)",
+              }}
+              onClick={createCourse}
+            >
+              Create Course
+            </Button>
+          )}
         </Grid>
         <Grid item style={{ width: "90%" }}>
           <Typography variant="h4">Course Title:</Typography>
@@ -209,6 +285,19 @@ Example call createCourse("yaboi", "id", "1 minute", "Abstract", [{title: "link2
             variant="outlined"
             defaultValue={title}
             onChange={(e) => setTitle(e.target.value)}
+          />
+        </Grid>
+        <Grid item style={{ width: "90%" }}>
+          <Typography variant="h6">Course Author:</Typography>
+        </Grid>
+        <Grid item className={classes.gridItem} style={{ marginTop: "1%" }}>
+          <TextField
+            className={classes.textInput}
+            id="outlined-basic"
+            label="Course Author"
+            variant="outlined"
+            defaultValue={author}
+            onChange={(e) => setAuthor(e.target.value)}
           />
         </Grid>
         <Grid item style={{ width: "90%" }}>
