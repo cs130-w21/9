08287fa47fb196697651e26e7019e38dae3cc591
dbreon88 <37003326+dbreon88@users.noreply.github.com>
2021-03-07 19:01:45 -0800

    restyled the login component to be centered, easier to use, uses material ui (#89)

--- opencourse-app/src/login.js
+++ opencourse-app/src/login.js
@@ -1,91 +1,110 @@
-import React, {useState, useEffect} from "react";
-import fire from './fire'
-import LoginComponent from './Components/LoginComponent';
-import CourseList from './Components/CourseList';
+import React, { useState, useEffect } from "react";
+import fire from "./fire";
+import LoginComponent from "./Components/LoginComponent";
+import CourseList from "./Components/CourseList";
+import { Container, Grid } from "@material-ui/core";
 
 export default function Login() {
-    const [user, setUser] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [passwordError, setPassWordError] = useState('');
-    const [hasAccount, setHasAccount] = useState(false);
-    const clearInputs = () => {
-        setEmailError("");
-        setPassword("");
-    }
-    const clearErrors = () => {
-        setEmailError('');
-        setPassWordError('');
-    }
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPassWordError] = useState("");
+  const [hasAccount, setHasAccount] = useState(false);
+  const clearInputs = () => {
+    setEmailError("");
+    setPassword("");
+  };
+  const clearErrors = () => {
+    setEmailError("");
+    setPassWordError("");
+  };
 
-const handleLogin = () => {
+  const handleLogin = () => {
     clearErrors();
-    fire.auth().signInWithEmailAndPassword(email, password).catch(err => {
-        switch(err.code){
-            case "auth/invalid-email":
-            case "auth/user-disabled":
-            case "auth/user-not-found":
-                setEmailError(err.message);
-                break;
-            case "auth/wrong-password":
-                setPassWordError(err.message);
-                break;
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPassWordError(err.message);
+            break;
         }
-    });
-};
-const handleSignup = () => {
+      });
+  };
+  const handleSignup = () => {
     clearErrors();
-    fire.auth().createUserWithEmailAndPassword(email, password).catch(err => {
-        switch(err.code){
-            case "auth/email-already-in-use":
-            case "auth/invalid-email":
-                setEmailError(err.message);
-                break;
-            case "auth/weak-password":
-                setPassWordError(err.message);
-                break;
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/weak-password":
+            setPassWordError(err.message);
+            break;
         }
-    });
-};
+      });
+  };
 
-const handleLogout = () => {
+  const handleLogout = () => {
     fire.auth().signOut();
-}
+  };
 
-const authListener = () => {
-    fire.auth().onAuthStateChanged(user => {
-        if (user){
-            clearInputs();
-            setUser(user);
-            console.log(user);
-        }else{
-            setUser("");
-        }
-
+  const authListener = () => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        clearInputs();
+        setUser(user);
+        console.log(user);
+      } else {
+        setUser("");
+      }
     });
-};
+  };
 
-useEffect(() => {
+  useEffect(() => {
     authListener();
-},[]);
+  }, []);
 
-    return (
-        <div>
-            {user ? (
-                <CourseList handleLogout={handleLogout}/>
-            ) : (<LoginComponent 
-                email={email} 
-                setEmail={setEmail} 
-                password={password} 
-                setPassword={setPassword} 
-                handleLogin={handleLogin} 
-                handleSignup={handleSignup} 
-                hasAccount={hasAccount} 
-                setHasAccount={setHasAccount} 
-                emailError={emailError} 
-                passwordError={passwordError} />)
-            }
-        </div>
-    );
+  return (
+    <div>
+      {user ? (
+        <CourseList handleLogout={handleLogout} />
+      ) : (
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: "100vh" }}
+        >
+          <Grid item xs={3}>
+            <LoginComponent
+              email={email}
+              setEmail={setEmail}
+              password={password}
+              setPassword={setPassword}
+              handleLogin={handleLogin}
+              handleSignup={handleSignup}
+              hasAccount={hasAccount}
+              setHasAccount={setHasAccount}
+              emailError={emailError}
+              passwordError={passwordError}
+            />
+          </Grid>
+        </Grid>
+      )}
+    </div>
+  );
 }
