d825a02ba979fd9a44ab8696b848f0f626087d8e
Dylan <dbreon@ucla.edu>
2021-03-07 18:52:17 -0800

    restyled the login component to be centered, easier to use, uses material ui

--- opencourse-app/src/Components/LoginComponent.js
+++ opencourse-app/src/Components/LoginComponent.js
@@ -1,43 +1,77 @@
-import React from 'react';
-
-
+import React from "react";
+import { TextField, Button } from "@material-ui/core";
 
 const LoginComponent = (props) => {
-    const {email, 
+  const {
+    email,
     setEmail,
     password,
-    setPassword, 
-    handleLogin, 
-    handleSignup, 
+    setPassword,
+    handleLogin,
+    handleSignup,
     hasAccount,
-    setHasAccount, 
+    setHasAccount,
     emailError,
-    passwordError} = props;
-    return(
-        <section className="login">
-            <div className="loginContainer">
-                <label>UserName</label>
-                <input type="text" autoFocus required value={email} onChange={e => setEmail(e.target.value)}/>
-                <p className="errorMsg">{emailError}</p>
-                <label>Password</label>
-                <input type="password" required value={password} onChange={e => setPassword(e.target.value)}/>
-                <p className="errorMsg">{passwordError}</p>
-                <div className="btnContainer">
-                    {hasAccount ? (
-                        <>
-                        <button onClick={handleLogin}>Sign in</button>
-                        <p>Don't have an account ? <span onClick={() => setHasAccount(!hasAccount)}>Sign up</span></p>
-                        </>
-                    ) : (
-                        <>
-                        <button onClick={handleSignup}>Sign up</button>
-                        <p>Have an account ? <span onClick={() => setHasAccount(!hasAccount)}>Sign in</span></p>
-                        </>
-                    )}
-                </div>
-            </div>
-        </section>
-    )
-}
+    passwordError,
+  } = props;
+  return (
+    <section className="login">
+      <div className="loginContainer">
+        <label>Username: </label>
+        <br />
+        <br />
+        <TextField
+          id="outlined-basic"
+          label="Username"
+          variant="filled"
+          autoFocus
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {/* <input type="text" autoFocus required value={email} onChange={e => setEmail(e.target.value)}/> */}
+        <p className="errorMsg">{emailError}</p>
+        <label>Password: </label>
+        <br />
+        <br />
+        <TextField
+          type="password"
+          variant="filled"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p className="errorMsg">{passwordError}</p>
+        <div className="btnContainer">
+          {hasAccount ? (
+            <>
+              <Button variant="contained" onClick={handleLogin}>
+                Sign in
+              </Button>
+              <p>
+                Don't have an account ?{" "}
+                <button onClick={() => setHasAccount(!hasAccount)}>
+                  Sign up
+                </button>
+              </p>
+            </>
+          ) : (
+            <>
+              <Button variant="contained" onClick={handleSignup}>
+                Sign up
+              </Button>
+              <p>
+                Have an account ?{" "}
+                <button onClick={() => setHasAccount(!hasAccount)}>
+                  Sign in
+                </button>
+              </p>
+            </>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default LoginComponent;
